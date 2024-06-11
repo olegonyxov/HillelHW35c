56c66015efbdf28373cd6f44406ed1264d968498
olegonyxov <olegonyxov@gmail.com>
2024-06-11 23:57:17 +0300

    22

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,15 @@ function App() {
   
   const [isAuthorized, setIsAuthorized] = useState(false);
   const [token, setToken] = useState(null);
-  // if (localStorage.getItem("userToken")){
-  //   setIsAuthorized(true)
-  // }
+  if (localStorage.getItem("userToken")){
+    setIsAuthorized(true)
+  }
   
   const handleUserSignIn = (token) => {
     console.log(`I received token: ${token}`);
     setIsAuthorized(true);
     setToken(token);
-    // localStorage.setItem("userToken",JSON.stringify(token))
+    localStorage.setItem("userToken",JSON.stringify(token))
   }
 
   return (
