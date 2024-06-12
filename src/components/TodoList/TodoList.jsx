49faf40e24943cb4480c64eca457e2c26e871733
olegonyxov <olegonyxov@gmail.com>
2024-06-13 01:00:22 +0300

    222

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,20 +1,30 @@
-import axios from "axios";
-import { API_TODOS, API_URL } from "../../urls";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { API_TODOS, API_URL } from '../../urls';
 
-export default function TodoList({ values = [], auth,renew }) {
+export default function TodoList({ values = [], auth, renew }) {
+  const [editingItem, setEditingItem] = useState(null); // Состояние для отслеживания редактируемого элемента
+  const [editTitle, setEditTitle] = useState(''); // Состояние для хранения изменяемого заголовка
+
   async function removeItem(itemID) {
-    try {
       const { data } = await axios.delete(`${API_URL}${API_TODOS}/${itemID}`, {
         headers: {
-          'Authorization': auth 
-        }
+          Authorization: auth,
+        },
       });
       console.log(data);
       renew()
-    } catch (error) {
-      console.error('Error deleting todo:', error);
-     
-    }
+  }
+  async function updateItem(itemID) {
+      const { data } = await axios.put(`${API_URL}${API_TODOS}/${itemID}`, { title: editTitle }, {
+        headers: {
+          Authorization: auth,
+        },
+      });
+      console.log(data);
+      renew()
+      setEditingItem(null);
+    
   }
 
   return (
@@ -22,11 +32,26 @@ export default function TodoList({ values = [], auth,renew }) {
       <ul>
         {values.map(item => (
           <li key={item.id}>
-            {item.title} <input type="button" value="delete" onClick={() => removeItem(item.id)} />
+            {editingItem === item.id ? (
+              <div>
+                <input
+                  type="text"
+                  value={editTitle}
+                  onChange={(e) => setEditTitle(e.target.value)}
+                />
+                <button onClick={() => updateItem(item.id)}>Save</button>
+                <button onClick={() => setEditingItem(null)}>Cancel</button>
+              </div>
+            ) : (
+              <div>
+                {item.title}{' '}
+                <button onClick={() => removeItem(item.id)}>Delete</button>{' '}
+                <button onClick={() => setEditingItem(item.id)}>Edit</button>
+              </div>
+            )}
           </li>
         ))}
       </ul>
     </div>
   );
 }
-
